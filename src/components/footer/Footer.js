830a686a311090ee90d1154cbfdc51fb76a8396d
sandramtzd <s.martinez-dominguez@rgu.ac.uk>
2023-12-22 17:40:28 +0000

    set up components structure

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './footer.css'
 
-const App = () => {
+const Footer = () => {
   return (
     <div>
       
@@ -8,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Footer
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './nav.css'
 
-const App = () => {
+const Nav = () => {
   return (
     <div>
       
@@ -8,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Nav
